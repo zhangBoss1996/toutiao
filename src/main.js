c8b403610bed86040fe7abfd66cbe7a980a54b25
zhangboss <zhangboss>
2020-01-11 19:47:41 +0800

    头条的页面布局+vant+postcss-pxtorem+amfe-flexible

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入Vant 插件以及样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '@/styles/index.less'
+//  引入布局自动适配插件
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
