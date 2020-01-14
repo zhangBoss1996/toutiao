9f901e7ce283032afc73cdbc67f5bccf08763250
zhangboss <zhangboss>
2020-01-14 19:20:31 +0800

    上拉加载

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="scroll-wrapper">
+    <van-list v-model="upLoading" :finished="finished" @load="onLoad">
+        <van-cell v-for="articles in articles" :key='articles' :title="articles"></van-cell>
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      upLoading: false, // 是否加载数据 true 上拉加载数据 false关闭
+      finished: false, // 是否加载数据 当为true的时候加载完毕
+      articles: [] // 定义一个数组自己定义数据渲染页面
+    }
+  },
+  methods: {
+    onLoad () {
+      setTimeout(() => {
+        if (this.articles.length < 50) {
+        // 自己 模拟数据渲染页面 当页面的条数小于50条的时候渲染数据
+          let arr = Array.from(Array(10), (value, index) => this.articles.length + index + 1)
+          this.articles.push(...arr)
+          this.upLoading = false
+        } else {
+          this.finished = true // 告诉组件内容已经渲染完成 关闭加载
+        }
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,81 @@
 <template>
-  <div class="container">主页</div>
+  <div class="container">
+    <van-tabs v-model="activeIndex" swipeable>
+      <van-tab :title="'标签' + item" v-for="item in 10" :key="item">
+     <article-list></article-list>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
+import ArticleList from './components/article-list'
 export default {
+  components: {
+    ArticleList // 注册组件
+  },
+  data () {
+    return {
+      activeIndex: 0 // 默认启动第0个标签
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content {
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
